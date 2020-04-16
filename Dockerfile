2aecd58b181d82024dae4e326374046521b4a553
Kritsada Dechawantana <kritsada@perfenterprise.com>
2020-04-16 17:23:48 +0700

    Init project

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM mcr.microsoft.com/dotnet/core/sdk:3.1 as build
+WORKDIR /app
+COPY LineBackend/LineBackend.csproj .
+RUN dotnet restore
+COPY LineBackend LineBackend
+RUN dotnet build LineBackend
+
+
+FROM build as publish
+WORKDIR /app
+RUN dotnet publish LineBackend -c Release -o out -r linux-x64
+
+
+FROM mcr.microsoft.com/dotnet/core/runtime:3.1 AS runtime
+ENV DOTNET_USE_POLLING_FILE_WATCHER=true
+ENV PORT=80
+WORKDIR /app
+COPY --from=publish /app/out .
+CMD dotnet LineBackend.dll
---
+++ LineBackend/Controllers/PaymentController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace LineBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        [HttpGet("authorize")]
+        public async Task<IActionResult> Authorize(string transactionId, string orderId)
+        {
+            using (var client = new HttpClient())
+            {
+                var baseUrl = "https://api-pay.line.me";
+                var requestUrl = $"/v3/payments/{transactionId}/confirm";
+                var channelId = "1653957456";
+                var channelSecret = "c35bfea4d5ae857d9f1c15614c48e935";
+                var nonce = Guid.NewGuid().ToString();
+
+                var product = new ConfirmInfo
+                {
+                    Amount = 0.02,
+                    Currency = "THB"
+                };
+
+                var payload = JsonConvert.SerializeObject(product, new JsonSerializerSettings
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                });
+                var data = channelSecret + requestUrl + payload + nonce;
+                var hashHMAC = HashHMAC(channelSecret, data);
+
+                client.DefaultRequestHeaders.Add("X-LINE-ChannelId", channelId);
+                client.DefaultRequestHeaders.Add("X-LINE-Authorization-Nonce", nonce);
+                client.DefaultRequestHeaders.Add("X-LINE-Authorization", Convert.ToBase64String(hashHMAC));
+
+                var stringContent = new StringContent(payload, Encoding.UTF8, "application/json");
+                await client.PostAsync(baseUrl + requestUrl, stringContent);
+            }
+
+            return Ok();
+        }
+
+        private byte[] HashHMAC(string key, string data)
+        {
+            var hash = new HMACSHA256(Convert.FromBase64String(Convert.ToBase64String(Encoding.UTF8.GetBytes(key))));
+            return hash.ComputeHash(Convert.FromBase64String(Convert.ToBase64String(Encoding.UTF8.GetBytes(data))));
+        }
+    }
+
+
+    public class ConfirmInfo
+    {
+        public double Amount { get; set; }
+        public string Currency { get; set; }
+    }
+}
